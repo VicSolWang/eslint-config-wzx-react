a5e539f53896b6d95c988e6690e674865fdd9132
VICSOLWANG <413834325@qq.com>
2021-12-21 14:55:17 +0800

    feat: modify test

--- test.js
+++ test.js
@@ -1,27 +1,39 @@
 /**
- * Created by VicSolWang.
- * Date: 2020-02-16 11:35
+ * Created by VICSOLWANG.
+ * Date: 2020/02/16 11:35
  * Email: vic.sol.wang@gmail.com
  */
 
 const test = require('ava');
-const { CLIEngine } = require('eslint');
-const rule = require('./index');
+const { ESLint } = require('eslint');
+const config = require('./index');
 
 const isObject = (obj) => typeof obj === 'object' && obj !== null;
-
 const isArray = (array) => Array.isArray(array);
 
 test('Test basic properties of config.', (t) => {
-	t.true(isObject(rule.env));
-	t.true(isObject(rule.globals));
-	t.true(isObject(rule.parserOptions));
-	t.true(isArray(rule.plugins));
-	t.true(isObject(rule.rules));
+  t.true(isArray(config.extends) && config.extends.indexOf('airbnb') !== -1);
+  t.true(isArray(config.plugins));
+  t.true(isObject(config.rules));
+  if (config.overrides && config.overrides.length > 0) {
+    const overrideConfig = config.overrides[0] || {};
+    t.true(
+      isArray(overrideConfig.extends) &&
+        config.extends.indexOf('airbnb') !== -1 &&
+        overrideConfig.extends.indexOf('airbnb-typescript/base') !== -1,
+    );
+    t.true(
+      isObject(overrideConfig.parserOptions) &&
+        overrideConfig.parserOptions.project,
+    );
+    t.true(isArray(overrideConfig.plugins));
+    t.true(isObject(overrideConfig.rules));
+  }
 });
 
-test('Load config in eslint to validate all rule syntax is correct.', (t) => {
-	const cli = new CLIEngine();
-	const code = 'const foo = 1;\nconst bar = () => {};\nbar(foo);\n';
-	t.is(cli.executeOnText(code).errorCount, 0);
+test('Load config in eslint to validate all rule syntax is correct.', async (t) => {
+  const eslint = new ESLint();
+  const code = 'const a = 1;\nconst b = () => {};\nb(a);\n';
+  const results = await eslint.lintText(code);
+  t.is(((results || [])[0] || {}).errorCount, 0);
 });
